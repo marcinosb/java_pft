9931aeec9d4ff928fd6adf0c317702a0b7d6225b
marcinosb <marcinosb@gmail.com>
2016-12-07 01:02:08 +0100

    Changes after refactoring

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram5.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram5.java
@@ -2,6 +2,6 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram5 {
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World !");
 	}
 }
